4d5c9bd62b5a0870af124dc3d6de2465594a2576
周银锋 <zhouyfpg@126.com>
2024-01-15 23:17:26 +0800

    增加function component

--- vite-runner/App.jsx
+++ vite-runner/App.jsx
@@ -1,4 +1,15 @@
 import React from './corejs/React.js';
-const App=React.createElement("div",{id:'app'},'hi-','mini','react');
+// const App=React.createElement("div",{id:'app'},'hi-','mini','react');
+function Counter({num}){
+    return <div>counter:{num}</div>
+}
+
+const App = (
+    <div>
+        hi-mini-react
+        <Counter num={10}></Counter>
+        <Counter num={20}></Counter>
+    </div>
+)
 console.log(App);
 export default App;
--- vite-runner/corejs/React.js
+++ vite-runner/corejs/React.js
@@ -27,7 +27,11 @@ function createElement(type, props, ...children) {
         type,
         props: {
             ...props,
-            children: children.map(child => typeof child === 'string' ? createTextElement(child) : child)
+            children: children.map(
+                child => {
+                    const isTextNode = typeof child === 'string' || typeof child === 'number'
+                     return isTextNode ? createTextElement(child) : child
+                })
         }
     }
 }
@@ -69,22 +73,23 @@ function render(el, container) {
     root = nextWorkOfUnit;
 }
 function createDom(type) {
-    return type === 'TEXT_ELEMENT' 
-                    ? document.createTextNode("") 
-                    : document.createElement(type);
+    //TODO 这里怎么开箱 把函数转成DOM 原来开箱就是调用函数，返回对象
+    return type === 'TEXT_ELEMENT'
+        ? document.createTextNode("")
+        : document.createElement(type);
 }
 
-function updateProps(dom,props) {
+function updateProps(dom, props) {
     Object.keys(props).forEach(key => {
         if (key !== 'children') {
             dom[key] = props[key];
         }
     })
-    
+
 }
 
-function initChildrenList(fiber){
-    let children = fiber.props.children;
+function initChildrenList(fiber, children) {
+    // let children = fiber.props.children;
     let preChild = null
     children.forEach((child, index) => {
         //TODO 这个newFiber的child和sibling都没设置呢。后面怎么用的呢？
@@ -94,7 +99,7 @@ function initChildrenList(fiber){
             child: null,
             parent: fiber,
             sibling: null,
-            dom:null
+            dom: null
         }
         if (index === 0) {
             fiber.child = newFiber;
@@ -108,24 +113,36 @@ let nextWorkOfUnit = null;
 let root = null;
 //为什么performanceWork入参里不加一个container呢。对标render
 function performanceWork(fiber) {
-    if (!fiber.dom) {
-        //1.渲染dom
-        const dom = (fiber.dom) = createDom(fiber.type);
-        //2.处理props
-        updateProps(dom,fiber.props);
-        fiber.parent.dom.append(dom);
+    const isFunctionComponent = typeof fiber.type === 'function';
+    if (!isFunctionComponent) {
+        if (!fiber.dom) {
+            //1.渲染dom
+            const dom = (fiber.dom) = createDom(fiber.type);
+            //2.处理props
+            updateProps(dom, fiber.props);
+            //fiber.parent.dom.append(dom);
+        }
     }
 
+    const children = isFunctionComponent ? [fiber.type(fiber.props)] : fiber.props.children
     //3.转换链表
-    initChildrenList(fiber);
+    initChildrenList(fiber, children);
     //4.返回下次需要渲染的任务
     if (fiber.child) {
         return fiber.child
     }
-    if (fiber.sibling) {
-        return fiber.sibling
+    // if (fiber.sibling) {
+    //     return fiber.sibling
+    // }
+    //最后一个节点counter:10，因为10的parent的sibling是空，应该继续找上级，但是找到何时结束呢？答案一直找到根节点(ROOT节点的parent是undefind)
+    let nextFiber = fiber;
+    while(nextFiber){
+        if(nextFiber.sibling){
+          return   nextFiber.sibling;
+        }
+        nextFiber = nextFiber.parent;
     }
-    return fiber.parent.sibling;
+   // return fiber.parent.sibling;
 }
 function workLoop(IdleDeadline) {
     let shouldYield = false;
@@ -134,22 +151,29 @@ function workLoop(IdleDeadline) {
         nextWorkOfUnit = performanceWork(nextWorkOfUnit);
         shouldYield = IdleDeadline.timeRemaining() < 1;
     }
-    if(!nextWorkOfUnit && root){
+    if (!nextWorkOfUnit && root) {
         commitRoot(root.child);
     }
     requestIdleCallback(workLoop)
 }
 requestIdleCallback(workLoop);
 
-function commitRoot(fiber){
+function commitRoot(fiber) {
     commitWork(fiber);
     root = null;
 }
-function commitWork(fiber){
-    if(!fiber){
+function commitWork(fiber) {
+    if (!fiber) {
         return;
     }
-    fiber.parent.dom.append(dom);
+    let fiberParent = fiber.parent
+    while (!fiberParent.dom) {
+        fiberParent = fiberParent.parent
+    }
+    if (fiber.dom) {
+        fiberParent.dom.append(fiber.dom);
+    }
+
     commitWork(fiber.child);
     commitWork(fiber.sibling);
 
